1d786ae9408529f35d19cc110040ea2c6db5fe35
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-18 05:02:44 +0330

    Use redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,47 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import employeesReducer from "../features/employees/slice";
 import authReducer from "../features/auth/slice";
 import profileReducer from "../features/profile/slice";
 
-export const store = configureStore({
-  reducer: {
-    employees: employeesReducer,
-    auth: authReducer,
-    profile: profileReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  blacklist: [],
+};
+
+const rootReducer = combineReducers({
+  employees: employeesReducer,
+  auth: authReducer,
+  profile: profileReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
